a7a1d34591407f4f65930a92b33638a09f6647da
Mike Surdek <msurdek@gmail.com>
2021-02-11 21:56:01 -0500

    Add files via upload

---
+++ 10-1 Discussion Advancing Onward.R
@@ -0,0 +1,34 @@
+#Read In Data
+lifetime_earn <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/lifetime_earn.csv')
+student_debt <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/student_debt.csv')
+retirement <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/retirement.csv')
+home_owner <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/home_owner.csv')
+race_wealth <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/race_wealth.csv')
+income_time <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/income_time.csv')
+income_limits <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/income_limits.csv')
+income_aggregate <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/income_aggregate.csv')
+income_distribution <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/income_distribution.csv')
+income_mean <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/income_mean.csv')
+
+#Load packages
+library(tidyverse)
+install.packages("scales")
+
+#Data
+colnames(income_time)[2] <- "Percentile"
+income_time$income_family <- dollar_format(prefix = "$", suffix = "", largest_with_cents = 1, ..., big.mark = ",", negative_parens = FALSE)
+
+#Viz
+plot <- ggplot(income_time, aes(x = year, y = income_family)) +
+  geom_line(aes(color = Percentile)) +
+  geom_point(aes(color = Percentile)) +
+  geom_text(aes(label = ifelse(year == "1963",round(income_family),"")), hjust=0.5, vjust = -0.7, size = 3) +
+  geom_text(aes(label = ifelse(year == "2016", round(income_family),"")), hjust=0.5, vjust = -0.7, size = 3) +
+  expand_limits(y=c(0,200000)) +
+  theme_classic() +
+  theme(legend.position = "bottom") +
+  ylab("Average Family Income") +
+  xlab("Year") +
+  ggtitle("Income Inequality Over Time")
+
+plot
